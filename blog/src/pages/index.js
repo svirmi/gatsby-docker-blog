6918989fac2a0e387668af0ca54b50f5a0a7f1be
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-09-06 08:35:08 +0300

    navigation between pages

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from "react";
+import {Link} from "gatsby";
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div>
+        <div>
+            <h1>My Gatsby project</h1>
+            <Link to="/test/">Navigate to test page</Link>
+        </div>
+        <img src="https://media.giphy.com/media/woZVO2B8ZQ55DqKsmN/giphy.gif" alt="" />
+    </div>
+)
--- blog/src/pages/test.js
+++ blog/src/pages/test.js
@@ -1,9 +1,11 @@
 import React from "react";
+// import {navigate} from "gatsby";
 
 const Test = () => (
     <div>
-    <h1>This is a test page</h1>
+        <h1>This is a test page</h1>
         <p>Some paragraph text goes here</p>
+        <button onClick={() => console.log("navigate")}>Go to index page</button>
     </div>
 )
 
