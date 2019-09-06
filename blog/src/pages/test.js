05d65dbd428a388b5cb9d2790b6693fb2a67823e
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-09-06 08:36:44 +0300

    navigation between pages

--- blog/src/pages/test.js
+++ blog/src/pages/test.js
@@ -1,11 +1,11 @@
 import React from "react";
-// import {navigate} from "gatsby";
+import {navigate} from "gatsby";
 
 const Test = () => (
     <div>
         <h1>This is a test page</h1>
         <p>Some paragraph text goes here</p>
-        <button onClick={() => console.log("navigate")}>Go to index page</button>
+        <button onClick={() => navigate("/")}>Go to index page</button>
     </div>
 )
 
