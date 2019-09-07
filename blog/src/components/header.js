e403e993473ab4961db8443e7645a7b9845761b4
MS <mixa.ru.ru@gmail.com>
2019-09-07 14:17:05 +0300

    header.module.css

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import headerStyles from  "./header.module.css";
 
 export default (props) => (
     <div>
-        <h1>{props.title}</h1>
+        <h1 className={headerStyles.headerExample}>{props.title}</h1>
     </div>
 )
