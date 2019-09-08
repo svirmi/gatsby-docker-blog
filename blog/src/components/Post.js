f133f76720c59ef3822ac0b7ebd9d5a686937338
MS <mixa.ru.ru@gmail.com>
2019-09-08 19:40:27 +0300

    post styling

--- blog/src/components/Post.js
+++ blog/src/components/Post.js
@@ -1,18 +1,19 @@
 import React from "react";
 import {Card, Button} from "react-bootstrap";
 
-const Post = () => (
-    <Card style={{width: '24rem'}}>
-        <Card.Img variant="top" src="Brands4.jpg" />
-        <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-    </Card>
+const Post = (props) => (
+
+    <div className='p-3'>
+        <Card style={{}}>
+            <Card.Img variant="top" src="Brands4.jpg" />
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>{props.excerpt}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+    </div>
+
 );
 
 export default Post;
--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -7,16 +7,27 @@ import Post from "../components/Post";
 export default () => (
     <div>
         <Header title="Header of index page" />
-        <div className='container'>
-            <Post/>
-        </div>
+
+        <h1>My Gatsby project</h1>
 
         <div className='container'>
-            <div>
-                <h1>My Gatsby project</h1>
-                <Link to="/test/">Navigate to test page</Link>
+            <div className="row justify-content-md-center">
+                <div className='col-md-6'>
+                    <Post
+                        title="This is the first post"
+                        excerpt="Some other text here.."
+                    />
+                </div>
             </div>
-            <img src="https://media.giphy.com/media/woZVO2B8ZQ55DqKsmN/giphy.gif" alt="happy guy" />
+        </div>
+
+        <div className='container mt-4'>
+            <div className="row justify-content-md-center">
+                <div className='col-md-6'>
+                        <Link to="/test/">Navigate to test page</Link>
+                    <img src="https://media.giphy.com/media/woZVO2B8ZQ55DqKsmN/giphy.gif" alt="happy guy" />
+                </div>
+            </div>
         </div>
         <Footer />
     </div>
