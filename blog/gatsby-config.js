6b962ef8844d63575dc4f98d1a73646370c51000
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-09-13 08:42:53 +0300

    file source plugin installed

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -6,4 +6,23 @@
 
 module.exports = {
   /* Your site config here */
+    siteMetadata: {
+        title: "Gatsby-Bootstrap"
+    },
+    plugins: [
+        // You can have multiple instances of this plugin
+        // to read source nodes from different locations on your
+        // filesystem.
+        //
+        // The following sets up the Jekyll pattern of having a
+        // "pages" directory for Markdown files and a "data" directory
+        // for `.json`, `.yaml`, `.csv`.
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/`,
+            },
+        },
+        ]
 }
