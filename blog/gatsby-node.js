0fe3889e0d4e99d7f00a3c766937893e8ccd159b
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-09-14 13:41:41 +0300

    getting data

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -1,7 +1,35 @@
 const { createFilePath } = require("gatsby-source-filesystem");
 
-exports.onCreateNode = ({ node, getNode }) => {
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
   if (node.internal.type === "MarkdownRemark") {
-    console.log(createFilePath({ node, getNode, basePath: "posts" }));
+    const slug = createFilePath({ node, getNode, basePath: "posts" });
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug
+    });
   }
+};
+
+exports.createPages = ({ graphql, actions }) => {
+  return graphql(`
+      query{
+  allMarkdownRemark{
+    nodes{
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        image
+        date
+        keywords
+      }
+      excerpt
+      html
+    }
+  }
+}
+  `).then(result => console.log(JSON.stringify(result)))
 };
